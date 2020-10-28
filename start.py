8cc2064914f03d31d7d93e38b244bdd47a9b4d95
robotxwet <73596172+robotxwet@users.noreply.github.com>
2020-10-28 15:14:23 +0100

    Add files via upload

---
+++ start.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Przedmiot: Informatyka
+# Kierunek studiów: ???
+# Semestr: zimowy
+# Rok akademicki: 2020/2021
+# Data (dzień.miesiąc.rok): 31.11.2020
+# Imię: ???
+# Nazwisko: ???
+# Numer albumu ZUT: ???
+
+"""
+W tym miejscu przepisać temat.
+"""
