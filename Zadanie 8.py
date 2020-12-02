d5c2f1b80f5bf5440a9a25612558a43e9be9049b
robotxwet <73596172+robotxwet@users.noreply.github.com>
2020-12-02 18:06:20 +0100

    Update Zadanie 8.py

--- Zadanie 8.py
+++ Zadanie 8.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Przedmiot: Informatyka
+# Kierunek studiów: Energetyka
+# Semestr: zimowy
+# Rok akademicki: 2020/2021
+# Data (dzień.miesiąc.rok): 02.12.2020
+# Imię: Zbigniew
+# Nazwisko: Wereszczyński
+# Numer albumu ZUT: wz49676
+
+"""
+Napisz program który wygeneruje listę dziesięciu tysięcy pseudolosowych liczb całkowitych większych niż zero
+i mniejszych niż dwadzieścia tysięcy. Liczby nie mają się powtarzać. Oblicz ile wynosi średnia, czyli suma wszystkich
+wylosowanych liczb podzielona przez dziesięć tysięcy. Oblicz ile wynosi mediana,
+czyli taka z nich że połowa wylosowanych liczb jest mniejsza (lub równa) od niej, a połowa jest większa.
+"""
+
 Low = 1                                                         #Najmniejsza liczba do wygenerowania
 
 High = 20000                                                    #Największa liczba, której nie osiągniemy
